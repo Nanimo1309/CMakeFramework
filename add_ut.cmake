282ec64eac2a42dde888ff87592f942d538d706f
Jan Kot <nanimo1309@gmail.com>
2023-03-10 05:40:23 +0100

    Change interface dependency to public and add some security

--- add_ut.cmake
+++ add_ut.cmake
@@ -1,12 +1,12 @@
 function(add_ut)
     cmake_parse_arguments("" "" "" "SOURCE;DEPEND" ${ARGN})
 
-    add_executable(${MODULE_NAME}_UT ${_SOURCE})
-    target_link_libraries(${MODULE_NAME}_UT PRIVATE GTest::gtest_main ${MODULE_NAME} ${${MODULE_NAME}_DEPEND} ${_DEPEND})
+    add_executable(${module_name}_UT ${_SOURCE})
+    target_link_libraries(${module_name}_UT PRIVATE GTest::gtest_main ${module_name} ${module_depend} ${_DEPEND})
 
     include(GoogleTest)
-    gtest_discover_tests(${MODULE_NAME}_UT)
+    gtest_discover_tests(${module_name}_UT)
 
-    target_include_directories(${MODULE_NAME}_UT PRIVATE "${module_include}" "${module_src}")
+    target_include_directories(${module_name}_UT PRIVATE "${module_include}" "${module_src}")
 endfunction()
 
--- create_module.cmake
+++ create_module.cmake
@@ -1,32 +1,47 @@
 function(create_module)
     cmake_parse_arguments("" "" "" "SOURCE;DEPEND" ${ARGN})
 
-    get_filename_component(MODULE_NAME "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
-
-    set(module_interface "${CMAKE_CURRENT_BINARY_DIR}/module_interface")
-    file(MAKE_DIRECTORY "${module_interface}")
-    execute_process(COMMAND "${CMAKE_COMMAND}" -E create_symlink "${CMAKE_CURRENT_SOURCE_DIR}/include" "${module_interface}/${MODULE_NAME}")
-
-    string(REPLACE " " "_" MODULE_NAME ${MODULE_NAME})
+    get_filename_component(module_name "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
 
     set(module_include "${CMAKE_CURRENT_SOURCE_DIR}/include")
+    set(module_interface "${CMAKE_CURRENT_BINARY_DIR}/module_interface")
+    set(module_src "${CMAKE_CURRENT_SOURCE_DIR}/src")
+    set(module_tests "${CMAKE_CURRENT_SOURCE_DIR}/tests")
+    set(module_depend ${_DEPEND})
 
-    if(_SOURCE)
-        set(module_src "${CMAKE_CURRENT_SOURCE_DIR}/src")
+    if(IS_DIRECTORY "${module_include}")
+        file(MAKE_DIRECTORY "${module_interface}")
+        execute_process(COMMAND "${CMAKE_COMMAND}" -E create_symlink "${module_include}" "${module_interface}/${module_name}")
 
-        list(TRANSFORM _SOURCE PREPEND "${module_src}/")
+        string(REPLACE " " "_" module_name ${module_name})
+        
+        if(_SOURCE)
+            if(IS_DIRECTORY "${module_src}")
+                list(TRANSFORM _SOURCE PREPEND "${module_src}/")
 
-        add_library(${MODULE_NAME} ${_SOURCE})
-        target_include_directories(${MODULE_NAME} INTERFACE "${module_interface}" PRIVATE "${module_include}" "${module_src}")
+                add_library(${module_name} ${_SOURCE})
+                target_include_directories(${module_name} INTERFACE "${module_interface}" PRIVATE "${module_include}" "${module_src}")
+                target_link_libraries(${module_name} PRIVATE ${module_depend})
+            else()
+                message(SEND_ERROR "Sources given in ${module_name} module but there's no /src directory")
+            endif()
+        else()
+            add_library(${module_name} INTERFACE)
+            target_include_directories(${module_name} INTERFACE "${module_interface}")
+            target_link_libraries(${module_name} PUBLIC ${module_depend})
+        endif()
     else()
-        add_library(${MODULE_NAME} INTERFACE)
-        target_include_directories(${MODULE_NAME} INTERFACE "${module_interface}")
+        string(REPLACE " " "_" module_name ${module_name})
+
+        if(_SOURCE)
+            message(SEND_ERROR "Sources given in ${module_name} module but there's no /include directory")
+        endif()
+
+        add_custom_target(${module_name})
+        target_link_libraries(${module_name} PUBLIC ${module_depend})
     endif()
 
-    set(${MODULE_NAME}_DEPEND ${_DEPEND})
-    target_link_libraries(${MODULE_NAME} PRIVATE ${_DEPEND})
-
-    if(BUILD_TESTING AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/tests")
+    if(BUILD_TESTING AND IS_DIRECTORY "${module_tests}")
         add_subdirectory(tests)
     endif()
 endfunction()
