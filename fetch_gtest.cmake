17601e988ec9206a6c1a4e5d8db4ec651d76827c
Jan Kot <nanimo1309@gmail.com>
2023-07-16 00:50:30 +0200

    Change googletest tag to origin/main

--- fetch_gtest.cmake
+++ fetch_gtest.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     GTest
     GIT_REPOSITORY https://github.com/google/googletest
-    GIT_TAG 58d77fa8070e8cec2dc1ed015d66b454c8d78850
+    GIT_TAG origin/main
     GIT_SHALLOW TRUE
 )
 
